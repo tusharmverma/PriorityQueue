19433a20adb3e498b47fd251bbd982e635685921
Tushar Verma <tushar.m.verma@gmail.com>
2017-11-13 09:16:47 -0500

    Driver function in main

--- Priority Queue/main.cpp
+++ Priority Queue/main.cpp
@@ -1,1 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "PriorityQueue.hpp"
+#include "Node.h"
+using namespace std;
 
+// Driver of the program
+int main()
+
+{
+    
+    int choice, item, priority;
+    
+    PriorityQueue pq;
+    
+    do
+        
+    {
+        
+        cout<<"1.Insert\n";
+        
+        cout<<"2.Delete\n";
+        
+        cout<<"3.Display\n";
+        
+        cout<<"4.Quit\n";
+        
+        cout<<"Enter your choice : ";
+        
+        cin>>choice;
+        
+        switch(choice)
+        
+        {
+                
+            case 1:
+                cout<<"Input the item value to be added in the queue : ";
+                
+                cin>>item;
+                
+                cout<<"Enter its priority : ";
+                
+                cin>>priority;
+                
+                pq.insert(item, priority);
+                
+                break;
+                
+            case 2:
+                
+                pq.del();
+                
+                break;
+                
+            case 3:
+                
+                pq.display();
+                
+                break;
+                
+            case 4:
+                
+                break;
+                
+            default :
+                
+                cout<<"Wrong choice\n";
+                
+        }
+        
+    }
+    
+    while(choice != 4);
+    
+    return 0;
+    
+}
