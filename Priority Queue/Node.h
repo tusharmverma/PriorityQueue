4a65f966ed69a132cb94740e1f9509917b2c70b9
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 09:16:13 -0500

    Node Class

--- Priority Queue/Node.h
+++ Priority Queue/Node.h
@@ -1,1 +1,20 @@
+#ifndef Node_h
+#define Node_h
+#include "PriorityQueue.hpp"
+using namespace std;
+
+
+class Node
+{
+public:
+    int priority;
+    int info;
+    Node(int classID, int student){
+        info = classID;
+        priority = student;
+    }
+    Node* next;
+    
+};
+#endif /* Node_h */
 
